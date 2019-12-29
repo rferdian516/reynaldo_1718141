1b0327c147ef3f000edf0710d9b0da885d13337d
rferdian516 <rferdian516@gmail.com>
2019-12-29 11:13:21 +0700

    localization_activity

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -3,13 +3,31 @@ package com.example.tugas1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton language;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        language = findViewById(R.id.language);
+        language.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+            if(v.getId()==R.id.language){
+                Intent intent = new Intent (Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+            }
     }
 }
